3ab8e476f054ab0b293166d07c306cd293ce9d06
arctouch-brunopinheiro <bruno.pinheiro@arctouch.com>
2015-01-05 10:53:15 -0200

    Initial commit

---
+++ UaiTomationClient/UaiTomationClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UaiTomationClient
+//
+//  Created by Bruno Pinheiro on 1/5/15.
+//  Copyright (c) 2015 Bruno Pinheiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
